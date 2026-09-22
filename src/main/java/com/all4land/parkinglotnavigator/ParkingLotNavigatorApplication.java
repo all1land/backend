b2a3ec778c3c1746fbdbbc4ce7f7adb07be5b9a1
Eugene-Shin <tdw0312@gmail.com>
2026-09-22 16:07:14 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/all4land/parkinglotnavigator/ParkingLotNavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.all4land.parkinglotnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotNavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/all4land/parkinglotnavigator/ParkingLotNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all4land.parkinglotnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
