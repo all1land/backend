f1ffadab7836b2016ba37d33985a2a7505b96366
Eunbee <spiverain02@gmail.com>
2026-09-22 17:59:26 +0900

    feat: swagger 추가

---
+++ src/main/java/com/all4land/parkinglotnavigator/monitor/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.all4land.parkinglotnavigator;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI parkinglotnavigatorOpenApi() {
+		return new OpenAPI().info(new Info()
+				.title("Parking Lot Navigator BE API")
+				.description("Parking Lot Navigator backend API documentation")
+				.version("v1")
+				.contact(new Contact().name("ALL1Land"))
+				.license(new License().name("Internal Use")));
+	}
+}
