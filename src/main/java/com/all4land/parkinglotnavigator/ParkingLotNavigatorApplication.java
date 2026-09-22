eb3ceb48ba77d881d8319915c62fb0b2b35c3c14
Eugene-Shin <tdw0312@gmail.com>
2026-09-22 17:09:08 +0900

    settings: spotless 적용 및 코드 포맷팅

--- src/main/java/com/all4land/parkinglotnavigator/ParkingLotNavigatorApplication.java
+++ src/main/java/com/all4land/parkinglotnavigator/ParkingLotNavigatorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ParkingLotNavigatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingLotNavigatorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotNavigatorApplication.class, args);
+    }
 }
