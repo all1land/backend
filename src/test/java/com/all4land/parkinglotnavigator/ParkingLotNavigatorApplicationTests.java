9d4082a87643f0a74056053b6f91416748a56ca9
Eugene-Shin <tdw0312@gmail.com>
2026-09-23 11:54:40 +0900

    settings: 테스트 DB를 H2에서 Testcontainers(PostGIS)로 전환
    - H2 의존성 제거, spring-boot-testcontainers·testcontainers-postgresql 추가
    - global/config에 PostGIS 컨테이너 설정 추가, contextLoads 테스트에 적용
    - application-test.yml의 H2 접속 설정 제거

--- src/test/java/com/all4land/parkinglotnavigator/ParkingLotNavigatorApplicationTests.java
+++ src/test/java/com/all4land/parkinglotnavigator/ParkingLotNavigatorApplicationTests.java
@@ -1,11 +1,14 @@
 package com.all4land.parkinglotnavigator;
 
+import com.all4land.parkinglotnavigator.global.config.TestcontainersConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class ParkingLotNavigatorApplicationTests {
 
     @Test
